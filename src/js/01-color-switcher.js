83b9a6a58fcc79f8f88b3f421152b7a16f2978b4
ApolinarySzumski <apolinary@poczta.onet.pl>
2023-04-18 17:01:59 +0200

    finished 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop');
+const body = document.body;
+let intervalId = null;
 
+stopBtn.setAttribute('disabled', '');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const changeColor = () => {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled');
+};
+
+const stopChanging = () => {
+  clearInterval(intervalId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', '');
+};
+
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', stopChanging);
