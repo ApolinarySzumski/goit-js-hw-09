c68f1a6546fbeac937104119a881187a70c9cfa9
ApolinarySzumski <apolinary@poczta.onet.pl>
2023-05-05 12:11:05 +0200

    finished 2nd task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,10 +4,12 @@ import Notiflix from 'notiflix';
 
 const input = document.querySelector('#datetime-picker');
 const btn = document.querySelector('[data-start]');
-const dataDays = document.querySelector('[data-days]');
-const dataHours = document.querySelector('[data-hours]');
-const dataMinutes = document.querySelector('[data-minutes]');
-const dataSeconds = document.querySelector('[data-seconds]');
+const data = {
+  dataDays: document.querySelector('[data-days]'),
+  dataHours: document.querySelector('[data-hours]'),
+  dataMinutes: document.querySelector('[data-minutes]'),
+  dataSeconds: document.querySelector('[data-seconds]'),
+};
 
 btn.setAttribute('disabled', '');
 
@@ -17,52 +19,54 @@ flatpickr(input, {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] <= new Date()) {
-      Notiflix.Notify.failure('Please choose a date in the future');
-      btn.setAttribute('disabled', '');
-    } else {
-      btn.removeAttribute('disabled');
-    }
-    const timeAsNumber = new Date().getTime();
-    const selectedTimeAsNumber = selectedDates[0].getTime();
-    const difference = selectedTimeAsNumber - timeAsNumber;
-
-    const convertMsToDays = 1000 * 60 * 60 * 24;
-    const convertMsToHours = 1000 * 60 * 60;
-    const convertMsToMinutes = 1000 * 60;
-    const convertMsToSeconds = 1000;
-    console.log(convertMsToDays);
-    if (difference % convertMsToDays > 0) {
-      dataDays.textContent = Math.floor(difference / convertMsToDays);
-      if (
-        (difference - dataDays.textContent * convertMsToDays) %
-          convertMsToHours >
-        0
-      ) {
-        dataHours.textContent = Math.floor(
-          (difference - dataDays.textContent * convertMsToDays) /
-            convertMsToHours
-        );
-        if (
-          (difference -
-            dataDays.textContent * convertMsToDays -
-            dataHours.textContent * convertMsToHours) %
-            convertMsToMinutes >
-          0
-        ) {
-          dataMinutes.textContent = Math.floor(
-            (difference -
-              dataDays.textContent * convertMsToDays -
-              dataHours.textContent * convertMsToHours) /
-              convertMsToMinutes
-          );
-        }
-      }
-    }
-
-    console.log(timeAsNumber);
-    console.log(selectedTimeAsNumber);
-    console.log(difference);
+    changeOfDate(selectedDates);
     console.log(selectedDates[0]);
+
+    const CountdownTimer = () => {
+      setInterval(() => {
+        changeOfDate(selectedDates);
+      }, 1000);
+    };
+
+    btn.addEventListener('click', CountdownTimer);
   },
 });
+
+const changeOfDate = selectedDates => {
+  const now = new Date();
+  if (selectedDates[0] <= now) {
+    Notiflix.Notify.failure('Please choose a date in the future');
+    btn.setAttribute('disabled', '');
+  } else {
+    btn.removeAttribute('disabled');
+  }
+
+  let timeAsNumber = now.getTime();
+  let selectedTimeAsNumber = selectedDates[0].getTime();
+  let difference = selectedTimeAsNumber - timeAsNumber;
+
+  function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    return { days, hours, minutes, seconds };
+  }
+
+  const converted = convertMs(difference);
+
+  data.dataDays.textContent = converted.days.toString().padStart(2, '0');
+  data.dataHours.textContent = converted.hours.toString().padStart(2, '0');
+  data.dataMinutes.textContent = converted.minutes.toString().padStart(2, '0');
+  data.dataSeconds.textContent = converted.seconds.toString().padStart(2, '0');
+};
