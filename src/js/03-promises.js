9149d14eff4bb5907174eb5d7a6067c38534d428
ApolinarySzumski <apolinary@poczta.onet.pl>
2023-05-09 20:58:22 +0200

    try to do 3rd

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
+const btn = document.querySelector('[type = submit]');
+const inputDelay = document.querySelector('[name = delay]');
+const inputStep = document.querySelector('[name = step]');
+const inputAmount = document.querySelector('[name = amount]');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    return Promise.resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
     // Reject
+    return Promise.reject(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
+
+function createPromises() {
+  startingDelay = inputDelay.value;
+  delay = inputStep.value;
+  theAmount = inputAmount.value;
+  for (let position = 0; position <= theAmount; position++) {
+    createPromise(position, delay)
+      .then(msg => {
+        console.log(msg);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+}
+btn.addEventListener('click', createPromises);
